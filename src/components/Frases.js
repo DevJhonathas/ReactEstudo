8dfb57e42c75d950c6debbb19ebd926b71e1718f
Jhonathas <jhonathassouza8@gmail.com>
2022-07-14 17:47:54 -0300

    Iniciando curso

---
+++ src/components/Frases.js
@@ -0,0 +1,9 @@
+function Frases(){
+    return(
+        <div>
+            <p>Este é um component com uma frase.</p>
+        </div>
+    )
+}
+
+export default Frases
---
+++ src/components/HelloWorld.js
@@ -0,0 +1,13 @@
+import Frases from "./Frases"
+
+function HelloWorld(){
+
+    return (
+        <div>
+            <Frases />
+            <h1>My first component</h1>
+        </div>
+    )
+}
+
+export default HelloWorld
