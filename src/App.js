759f55b89de817647b9e94921a71c7c569f7b346
Jhonathas <jhonathassouza8@gmail.com>
2022-07-19 13:14:24 -0300

    DOC: Finalizando estudo sobre Renderização de listas

--- src/App.js
+++ src/App.js
@@ -4,15 +4,14 @@ import OutraLista from './components/OutraLista';
 
 
 function App() {
-
-  const meusItens = ['React','Vue','angular']
-
+  const meusItens = ['React','Vue','angular'];
   return (
     <div className="App">
       <h1>Redenrizacao de listas</h1>
       <OutraLista itens={meusItens} />
+      <OutraLista itens={[]} />
     </div>
   )
 }
 
-export default App;
+export default App
