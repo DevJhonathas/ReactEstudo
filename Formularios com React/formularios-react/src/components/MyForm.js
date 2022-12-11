33236c2a40d7911c9f350492395d1282b9760c65
Jhonathas <jhonathassouza8@gmail.com>
2022-12-11 17:33:49 -0300

    Estudando sobre form usando CSS no React

---
+++ Formularios com React/formularios-react/src/components/MyForm.js
@@ -0,0 +1,22 @@
+import './MyForm.css'
+const MyForm = () => {
+    return(
+        <div>
+            {/* 1- Criação de Form */}
+            <form>
+                <div>
+                    <label htmlFor="name">Nome:</label>
+                    <input type="text" name="name" placeholder='Digite o seu nome...'/>
+                </div>
+                {/* 2- Label envolvendo input - sugerida pela documentação do react*/}
+                <label>
+                    <span>E-mail</span>
+                    <input type="email" name="email" placeholder='Digite seu e-mail...' />
+                </label>
+                <input type="submit" value="Enviar" />
+            </form>
+        </div>
+    )
+}
+
+export default MyForm
