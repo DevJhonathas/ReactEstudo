c77c666d2e1673472aeae64d745e7b95f9f94617
Jhonathas <jhonathassouza8@gmail.com>
2022-12-14 23:59:08 -0300

    Feature: Gerenciamento de dados de input

--- Formularios com React/formularios-react/src/components/MyForm.js
+++ Formularios com React/formularios-react/src/components/MyForm.js
@@ -1,12 +1,22 @@
+import {useState} from 'react';
 import './MyForm.css'
+
 const MyForm = () => {
+    // 3 - Gerenciamento de dados
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
+
+    const handleName = (e) => {
+        setName(e.target.value);
+    };
+    console.log(name)
     return(
         <div>
             {/* 1- Criação de Form */}
             <form>
                 <div>
                     <label htmlFor="name">Nome:</label>
-                    <input type="text" name="name" placeholder='Digite o seu nome...'/>
+                    <input type="text" name="name" placeholder='Digite o seu nome...' onChange={handleName}/>
                 </div>
                 {/* 2- Label envolvendo input - sugerida pela documentação do react*/}
                 <label>
