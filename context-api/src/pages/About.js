8f92d4d4f4ba171a32365286954ab0fd1a09acd7
Jhonathas <jhonathassouza8@gmail.com>
2023-02-10 15:01:15 -0300

    Finalizando estudo sovre Context

--- context-api/src/pages/About.js
+++ context-api/src/pages/About.js
@@ -1,16 +1,24 @@
-import { useContext } from "react";
-import ChangeContext from "../component/ChangeContext";
-import {CounterContext} from "../context/CounterContext";
+// import { useContext } from "react";
+import ChangeCounter from "../component/ChangeCounter";
 
+// import {CounterContext} from "../context/CounterContext";
+import { useCounterContext } from "../hooks/useCounterContext";
+
+import { useTitleColorContext } from "../hooks/useTitleColorContext";
 
 const About = () => {
-    const {counter} = useContext(CounterContext);
+    // const {counter} = useContext(CounterContext);
+    const {counter} = useCounterContext();
+
+    // 5- Context mais complexo
+    const {color} = useTitleColorContext();
+
     return(
     <div>
-        <h1>About</h1>
+        <h1 style={{color: color}}>About</h1>
         <p>valor do contador no About: {counter}</p>
         {/* 3 - alterando valores contexto */}
-        <ChangeContext />
+        <ChangeCounter />
     </div>
     );
 };
